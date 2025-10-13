d100a2f08bf9e7ce9a9f4dfc8c6b9de1c5b79eac
LAPTOP-SN4FTV4P\hodag <hoda.gudarzi@gmail.com>
2025-10-13 09:43:33 +0330

    Basic API setup and reusable fetch hook

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,7 +1,27 @@
+import useFetch from "./hooks/useFetch";
+
+interface Product {
+  id: number;
+  title: string;
+  price: number;
+}
+
 export default function App() {
+  const { data: products, loading, error } = useFetch<Product[]>("/products");
+
+  if (loading) return <h3>Loading...</h3>;
+  if (error) return <h3>Error: {error}</h3>;
+
   return (
-    <div>
-      <h1>My Store Project</h1>
+    <div style={{ padding: "20px" }}>
+      <h1>Products (via useFetch)</h1>
+      <ul>
+        {products?.map((p) => (
+          <li key={p.id}>
+            {p.title} – ${p.price}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
